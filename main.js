d7138e0f367e9a2e1960da681c05b888bd719b6a
MatthewCodes123 <84956326+MatthewCodes123@users.noreply.github.com>
2021-09-07 17:15:42 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var names_of_people = [];
 function submit()
 {
     var GuestName = document.getElementById("name1").value;
-	// use the push function as - names_of_people.push(GuestName);
+	names_of_people.push(GuestName);
 	console.log(GuestName);    
     console.log(names_of_people);
     var lenght_of_name = names_of_people.length;
@@ -18,7 +18,7 @@ function show()
 	var i= names_of_people.join("<br>");
 	console.log(names_of_people);
 	document.getElementById("p1").innerHTML=i.toString();
-	document.getElementById("show").style.display="block";
+	document.getElementById("sort_button").style.display="block";
 	// display the name in the id= sort_button
 	}
 
